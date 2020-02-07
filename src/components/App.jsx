da642fdd335ebd3a06403eff4836521ea5c1ea69
eurioh <euri.elliot.oh@gmail.com>
2020-02-06 21:43:47 -0500

    add Ternary Operator in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,21 +2,26 @@ import React from "react";
 import LogIn from "./LogIn";
 
 
-var isLoggedIn = false;
+var isLoggedIn = true;
 
-function renderConditionally(){
- if(isLoggedIn === true){
-  return <h1>Hello</h1>;
- }else{
-   return <LogIn />;
- }
-}
+//following statement is too long turn this into expression and put it into App()
+// function renderConditionally(){
+//  if(isLoggedIn === true){
+//   return <h1>Hello</h1>;
+//  }else{
+//    return <LogIn />;
+//  }
+// }
 
 
 function App() {
   return (
     <div className="container">
-      {renderConditionally()}
+      {/* {renderConditionally()} */} 
+
+      {/* Ternary Operator - turn if statment into an expression */}
+      {isLoggedIn ? <h1>Hello </h1> : <LogIn />};
+
     </div>
   );
 }
