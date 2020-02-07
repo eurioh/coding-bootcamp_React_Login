d3f4568c2885f9f47d0bfdfd68d6875ccb575717
eurioh <euri.elliot.oh@gmail.com>
2020-02-06 22:00:06 -0500

    Ternary Operator Practice and AND Operator refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,27 @@
 import React from "react";
-import LogIn from "./LogIn";
+// import LogIn from "./LogIn";
 
 
 var isLoggedIn = true;
 
-//following statement is too long turn this into expression and put it into App()
-// function renderConditionally(){
-//  if(isLoggedIn === true){
-//   return <h1>Hello</h1>;
-//  }else{
-//    return <LogIn />;
-//  }
-// }
+const currentTime = new Date(2019, 11, 1, 9).getHours();
+
+console.log(currentTime);
 
 
 function App() {
   return (
     <div className="container">
-      {/* {renderConditionally()} */} 
-
-      {/* Ternary Operator - turn if statment into an expression */}
-      {isLoggedIn ? <h1>Hello </h1> : <LogIn />};
-
-    </div>
-  );
+      {/* {isLoggedIn ? <h1>Hello </h1> : <LogIn />}; */}
+      
+      {/* Ternary Operator practice */}
+      {/* {currentTime > 12 ? <h1> Why are you still working? </h1> : null} */}
+      
+      {/* Much shorter ways for Ternary Operator expression - AND Operator*/}
+      {currentTime > 12 && <h1> Why are you still working? </h1>}
+      {/* in &&, if left hand is false, the result is anyway false. So the next expression never render */}
+    </div>);
+  
 }
 
 export default App;
